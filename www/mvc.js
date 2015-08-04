94cba22613711a663231db96c10f6d80a0fa1d16
aHirokiKumamoto <hiroki.kumamoto@access-company.com>
2015-08-04 18:31:18 +0900

    Add echo method

--- www/mvc.js
+++ www/mvc.js
@@ -3,5 +3,8 @@ var exec = require('cordova/exec');
 module.exports = {
   coolMethod: function(arg0, success, error) {
     //exec(success, error, "mvc", "coolMethod", [arg0]);
+  },
+  echo: function(str) {
+    return str;
   }
 };
